4cde7b88addfb5c6e050d88bd6b81c0051651747
AutumnLeaf742002 <alexismanuelperezperez742002@gmail.com>
2022-10-27 18:19:44 -0400

    Buscador de clietnes listo
    .

--- template/backend/js/get-customers.js
+++ template/backend/js/get-customers.js
@@ -25,3 +25,33 @@ function perfil_cliente(id_cl, id_co)
     window.location.href = "user-profile-customer.php?id_cl="+id_cl+"&id_co="+id_co
     
 }
+
+function get_customers_by_input()
+{
+    let buscador_cliente = document.getElementById('buscador-cliente')
+    let select_buscador = document.getElementById('select-buscador')
+
+    console.log('entra')
+    
+    if(select_buscador.value == 'todos')
+    {
+        get_customers()
+    }
+    else
+    {
+        if(buscador_cliente.value.length > 0)
+        {
+            let request = new XMLHttpRequest()
+            request.open('POST', 'backend/php/get-customer-by-input.php', true)
+            request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+            request.onreadystatechange = function () {
+
+                if(request.readyState == 4 && request.status == 200) {
+                    contenedor_clientes.innerHTML = request.responseText
+                }
+            }
+
+            request.send(`search=${buscador_cliente.value}&filtro=${select_buscador.value}`)
+        }
+    }
+}
---
+++ template/backend/php/get-customer-by-input.php
@@ -0,0 +1,70 @@
+<?php
+
+    if(isset($_POST))
+    {
+        $search = $_POST['search'] ?? "nada";
+        $filtro = $_POST['filtro'] ?? "nada";
+
+        include_once "connection.php";
+        include_once "commands.php";
+        
+        $oCon = connect();
+        $sql = "SELECT * FROM clientes WHERE $filtro LIKE '$search%' ";
+
+        $res = select($oCon, $sql);
+
+        if(count($res) > 0)
+        {
+            foreach($res as $item)
+            {
+                $n_serie = $item["N_serie_cliente"];
+                $res_n_serie = select($oCon, "SELECT Id FROM co_aplicantes WHERE C_N_serie_cliente = '$n_serie'");
+
+                
+                if(count($res_n_serie) > 0)
+                {
+                    $id_co = $res_n_serie[0]["Id"];
+                }
+                else
+                {
+                    $id_co = 0;
+                }
+
+                echo'
+                    <tr class="hover" style="cursor: pointer;" onclick="perfil_cliente('.$item["Id"].', '.$id_co.')">
+                        <td>
+                            <img src="Avatars/avatar-'.$item["Avatar"].'.svg" class="d-inline-block img-circle " alt="tbl">
+                        </td>
+                        <td class="pro-name">
+                            '.$item["Primer_nombre"].'
+                        </td>
+                        <td>
+                            '.$item["N_seguro_social"].'
+                        </td>
+                        <td>
+                            '.$item["N_licencia_conducir"].'
+                        </td>
+                        <td>
+                            '.$item["Estado"].'
+                        </td>
+                        <td>
+                            '.$item["Vencimiento"].'
+                        </td>
+                        <td>
+                            '.$item["Direccion"].'
+                        </td>
+                        
+                    </tr>';
+            }
+
+            echo '<a id="final"></a>';
+        }
+        else
+        {
+            // si no hay nada en la tabla, devuelve none
+            echo '';
+        }
+        echo "";
+    }
+
+?>
