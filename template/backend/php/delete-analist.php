e8931d2daef09748397c6e038459336652467dcf
AutumnLeaf742002 <alexismanuelperezperez742002@gmail.com>
2023-01-04 21:06:43 -0400

    .
    .

--- template/backend/php/delete-analist.php
+++ template/backend/php/delete-analist.php
@@ -7,7 +7,7 @@
 
         $oCon = connect();
         $id = $_POST["id"]??0;
-        define("sql", "DELETE FROM analyst WHERE Id = ".$id);
+        define("sql", "DELETE FROM analyst WHERE Id = ".$id."; CALL sp_reset_auto();");
         $res = command($oCon, sql);
 
         echo $res;
